00618b9eb7c1c72b5f564d3ed139698bffaee801
riperiperi <rhy3756547@hotmail.com>
2020-11-21 03:20:02 +0000

    Interrupt GPU command processing when a frame's fence is reached.

--- Ryujinx.HLE/HOS/Services/SurfaceFlinger/SurfaceFlinger.cs
+++ Ryujinx.HLE/HOS/Services/SurfaceFlinger/SurfaceFlinger.cs
@@ -292,6 +292,12 @@ namespace Ryujinx.HLE.HOS.Services.SurfaceFlinger
                 Item  = item,
             };
 
+            item.Fence.RegisterCallback(_device.Gpu, () => 
+            {
+                _device.Gpu.Window.SignalFrameReady();
+                _device.Gpu.GPFifo.Interrupt();
+            });
+
             _device.Gpu.Window.EnqueueFrameThreadSafe(
                 frameBufferAddress,
                 frameBufferWidth,
